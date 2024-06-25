841559d342049bb6cfba461aa8bef4b10648c93f
Юлия Кунгурова <uliakungurova@MacBook-Pro-Ulia-2.local>
2024-06-25 15:10:05 +0300

    add alg prima

--- src/core/s21_graph_algorithms.h
+++ src/core/s21_graph_algorithms.h
@@ -14,7 +14,7 @@ public:
   int GetShortestPathBetweenVertices(s21::Graph &graph, size_t from, size_t to);
   matrix_uint32_t GetShortestPathsBetweenAllVertices(Graph &graph);
   void AdjacencyMatrixPrepare(matrix_uint32_t &adjacency_matrix, size_t size);
-  bool CheckCorrectVertex(int vertex1, int vertex2, int size);
+  matrix_uint32_t GetLeastSpanningTree(Graph &graph);
 };
 
 } // namespace s21
--- src/tests/s21_algorithms_tests.cc
+++ src/tests/s21_algorithms_tests.cc
@@ -122,6 +122,51 @@ TEST(GraphAlgorithms, GetShortestPathsBetweenAllVertices_FromFile_1) {
   check_matrix[5][4] = 9;
   check_matrix[5][5] = 0;
 
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < size; j++) {
+      EXPECT_EQ(check_matrix[i][j], res_matrix[i][j]);
+    }
+  }
+}
+
+TEST(GraphAlgorithms, GetLeastSpanningTree_FromFile_1) {
+  s21::Graph graph;
+  graph.LoadGraphFromFile("examples/valid_matrix_4.txt");
+  size_t size = graph.GetSize();
+
+  s21::GraphAlgorithms graph_alg;
+  s21::GraphAlgorithms::matrix_uint32_t res_matrix = graph_alg.GetLeastSpanningTree(graph);
+  s21::GraphAlgorithms::matrix_uint32_t check_matrix(size, std::vector<uint32_t>(size));
+
+  check_matrix[0][0] = 0;
+  check_matrix[0][1] = 9;
+  check_matrix[0][2] = 0;
+  check_matrix[0][3] = 0;
+  check_matrix[0][4] = 0;
+  
+  check_matrix[1][0] = 0;
+  check_matrix[1][1] = 0;
+  check_matrix[1][2] = 0;
+  check_matrix[1][3] = 19;
+  check_matrix[1][4] = 0;
+
+  check_matrix[2][0] = 0;
+  check_matrix[2][1] = 0;
+  check_matrix[2][2] = 0;
+  check_matrix[2][3] = 0;
+  check_matrix[2][4] = 0;
+
+  check_matrix[3][0] = 0;
+  check_matrix[3][1] = 0;
+  check_matrix[3][2] = 51;
+  check_matrix[3][3] = 0;
+  check_matrix[3][4] = 31;
+
+  check_matrix[4][0] = 0;
+  check_matrix[4][1] = 0;
+  check_matrix[4][2] = 0;
+  check_matrix[4][3] = 0;
+  check_matrix[4][4] = 0;
 
   // for (size_t i = 0; i < size; i++) {
   //   for (size_t j = 0; j < size; j++) {
