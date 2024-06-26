f8a91685ef59cfb6db0df568c5b2ec5726f6e0b5
erosrolf <erosrolf@gmail.com>
2024-06-26 12:26:52 +0300

    added some tests on DFS and BFS

--- src/tests/s21_alhorithms_search_test.cc
+++ src/tests/s21_alhorithms_search_test.cc
@@ -2,10 +2,6 @@
 #include <_types/_uint32_t.h>
 #include <gtest/gtest.h>
 
-// vector<uint32_t> DepthFirstSearch(const Graph &graph, uint32_t start_vertex);
-// vector<uint32_t> BreadthFitstSeatch(const Graph &graph,
-//                                     uint32_t start_vertex);
-
 TEST(GraphAlgorithmsSearch, DepthFirstSearch_01) {
   s21::Graph graph;
   s21::GraphAlgorithms graphAlgorithms;
@@ -17,6 +13,17 @@ TEST(GraphAlgorithmsSearch, DepthFirstSearch_01) {
   }
 }
 
+TEST(GraphAlgorithmsSearch, DepthFirstSearch_02) {
+  s21::Graph graph;
+  s21::GraphAlgorithms graphAlgorithms;
+  graph.LoadGraphFromFile("examples/deep_graph.txt");
+  std::vector<uint32_t> expected_vector = {1, 2, 3, 4};
+  auto result_vector = graphAlgorithms.DepthFirstSearch(graph, 1);
+  for (size_t i = 0; i < result_vector.size(); ++i) {
+    EXPECT_EQ(expected_vector[i], result_vector[i]);
+  }
+}
+
 TEST(GraphAlgorithmsSearch, BreadthFitstSeatch_01) {
   s21::Graph graph;
   s21::GraphAlgorithms graphAlgorithms;
@@ -24,6 +31,17 @@ TEST(GraphAlgorithmsSearch, BreadthFitstSeatch_01) {
   std::vector<uint32_t> expected_vector = {4, 1, 3, 5, 2};
   auto result_vector = graphAlgorithms.BreadthFitstSearch(graph, 4);
   for (size_t i = 0; i < expected_vector.size(); ++i) {
+    EXPECT_EQ(expected_vector[i], result_vector[i]);
+  }
+}
+
+TEST(GraphAlgorithmsSearch, BreadthFirstSearch_02) {
+  s21::Graph graph;
+  s21::GraphAlgorithms graphAlgorithms;
+  graph.LoadGraphFromFile("examples/deep_graph.txt");
+  std::vector<uint32_t> expected_vector = {1, 2, 3, 4};
+  auto result_vector = graphAlgorithms.BreadthFitstSearch(graph, 1);
+  for (size_t i = 0; i < result_vector.size(); ++i) {
     EXPECT_EQ(expected_vector[i], result_vector[i]);
   }
 }
